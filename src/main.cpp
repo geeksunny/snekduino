8a635f7a68ea07212d662ffbb8308827086173ba
Justin Swanson <geeksunny@gmail.com>
2021-08-03 20:13:52 -0500

    Serial input for user control

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,20 @@ extern "C" {
 #include "snek.inc"
 }
 
+#define CHAR_ESCAPE_PREFIX    27
+#define CHAR_ESCAPE_SEQUENCE  91
+#define CHAR_ARROW_UP         65
+#define CHAR_ARROW_DOWN       66
+#define CHAR_ARROW_LEFT       68
+#define CHAR_ARROW_RIGHT      67
+
+enum class KeyboardInput {
+  UP, DOWN, LEFT, RIGHT, INVALID, NONE
+};
+
+char input;
+KeyboardInput inputKey = KeyboardInput::NONE;
+
 CRGB leds[NUM_LEDS];
 
 CRGBPalette16 currentPalette;
@@ -15,6 +29,43 @@ void FillLEDsFromPaletteColors(uint8_t colorIndex) {
   for (auto & led : leds) {
     led = ColorFromPalette(currentPalette, colorIndex, BRIGHTNESS, currentBlending);
     colorIndex += 3;
+  }
+}
+
+KeyboardInput readInput() {
+  if (Serial.available()) {
+    input = Serial.read();
+    if (input == CHAR_ESCAPE_PREFIX) {
+      bool awaiting_sequence_char = true;
+      while (true) {
+        while (!Serial.available()) { /* Wait */ }
+        input = Serial.read();
+        if (awaiting_sequence_char) {
+          if (input == CHAR_ESCAPE_SEQUENCE) {
+            awaiting_sequence_char = false;
+          } else {
+            return KeyboardInput::INVALID;
+          }
+        } else {
+          switch (input) {
+            case CHAR_ARROW_UP:
+              return KeyboardInput::UP;
+            case CHAR_ARROW_DOWN:
+              return KeyboardInput::DOWN;
+            case CHAR_ARROW_LEFT:
+              return KeyboardInput::LEFT;
+            case CHAR_ARROW_RIGHT:
+              return KeyboardInput::RIGHT;
+            default:
+              return KeyboardInput::INVALID;
+          }
+        }
+      }
+    } else {
+      return KeyboardInput::INVALID;
+    }
+  } else {
+    return KeyboardInput::NONE;
   }
 }
 
@@ -45,4 +96,25 @@ void loop() {
   FastLED.delay(1000 / REFRESH_RATE);
 
   snake_exec();
+
+  inputKey = readInput();
+  switch (inputKey) {
+    case KeyboardInput::UP:
+      Serial.println("UP ARROW PRESSED!");
+      break;
+    case KeyboardInput::DOWN:
+      Serial.println("DOWN ARROW PRESSED!");
+      break;
+    case KeyboardInput::LEFT:
+      Serial.println("LEFT ARROW PRESSED!");
+      break;
+    case KeyboardInput::RIGHT:
+      Serial.println("RIGHT ARROW PRESSED!");
+      break;
+    case KeyboardInput::INVALID:
+      Serial.println("INVALID INPUT.");
+      break;
+    case KeyboardInput::NONE:
+      break;
+  }
 }
